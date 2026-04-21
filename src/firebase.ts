0ca6cd9705618d756b4979bad557fd20cbeb28ec
Eryk Sandecki <eryk.sandecki@gmail.com>
2026-04-21 21:03:10 +0200

    BP: firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,20 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA00NttEzoNHqbtlPeh1yGIur5eU4eDsMo",
-  authDomain: "bouw-pro-94db2.firebaseapp.com",
-  projectId: "bouw-pro-94db2",
-  storageBucket: "bouw-pro-94db2.firebasestorage.app",
-  messagingSenderId: "978865228058",
-  appId: "1:978865228058:web:3bf90c55c0b126381a44a3",
-  measurementId: "G-H6BTT14WQF",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export { app, analytics };
